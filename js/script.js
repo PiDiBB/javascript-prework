e5fe950762bb65ac65b66773b8d3d161605871e6
Przemyslaw Damek <pdamek.bb@gmail.com>
2022-02-03 17:52:09 +0100

    Combine computerMove draw

--- js/script.js
+++ js/script.js
@@ -9,3 +9,17 @@ printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wy
 var playerMove;
 playerMove = 'papier';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else {
+  computerMove = 'nożyce';
+}
+printMessage('Mój ruch: ' + computerMove);
+
+Math.random();
