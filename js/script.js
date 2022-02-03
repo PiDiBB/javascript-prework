583b1bb771dd7df70163efe0df6af66428af4341
Przemyslaw Damek <pdamek.bb@gmail.com>
2022-02-03 18:09:42 +0100

    Add playerMove answers

--- js/script.js
+++ js/script.js
@@ -23,3 +23,17 @@ if (randomNumber == '1') {
 printMessage('Mój ruch: ' + computerMove);
 
 Math.random();
+
+var playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  playerMove = 'Błędny wybór - wybierz: 1, 2 lub 3';
+}
+printMessage('Twój ruch: ' + playerMove);
